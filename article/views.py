61df7e866c4e7bfcf1d41fb6d0146ac2e832ff95
Joshua Raichur <hello@joshuaraichur.com>
2015-01-07 07:47:22 +0530

    basic views for the articles

--- article/views.py
+++ article/views.py
@@ -1,26 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template.loader import get_template
 from django.shortcuts import render_to_response
-from django.template import Context
-from django.views.generic.base import TemplateView
+from article.models import Article
 
-def hello(request):
-    name = "Josh"
-    html = "<html><body>Hi %s. This seems to have worked</body></html>" % name
-    return HttpResponse(html)
+def articles(request):
+    return render_to_response('articles.html',
+                              {'articles': Article.objects.all()})
 
-def hello_template(request):
-    name = "Josh"
-    t = get_template('hello.html')
-    html = t.render(Context({'name': name}))
-    return HttpResponse(html)
-
-class HelloTemplate(TemplateView):
-
-    template_name = 'hello_class.html'
-
-    def get_context_data(self, **kwargs):
-        context = super(HelloTemplate, self).get_context_data(**kwargs)
-        context['name'] = 'Josh'
-        return context
+def article(request, article_id=1):
+    return render_to_response('article.html',
+                              {'article': Article.objects.get(id=article_id)})
--- wiki/urls.py
+++ wiki/urls.py
@@ -1,11 +1,5 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
-from article.views import HelloTemplate
 
 urlpatterns = patterns('',
-    url(r'^hello/$', 'article.views.hello'),
-    url(r'^hello_template/$', 'article.views.hello_template'),
-    url(r'^hello_template_simple/$', 'article.views.hello_template_simple'),
-    url(r'^hello_class_view/$', HelloTemplate.as_view()),
-    url(r'^admin/', include(admin.site.urls)),
+    (r'^articles/', include('article.urls')),
 )
