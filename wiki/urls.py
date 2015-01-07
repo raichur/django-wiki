79641c5d64f6013f150b488cf2ebdb18e56b1dc4
Joshua Raichur <hello@joshuaraichur.com>
2015-01-07 08:14:40 +0530

    django admin article

--- wiki/urls.py
+++ wiki/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^articles/', include('article.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 )
